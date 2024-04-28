b76e968eff1aea1d8013c62287fd8649acbd37a0
Hubert Madej <hubertmadej02@gmail.com>
2024-04-28 11:58:56 +0200

    Firebase Web Client setup

---
+++ web-client/app/utilities/firebase/firebase.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+import { firebaseConfig } from "../config/firebase.config";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A Prommise that resolves with the user's data.
+ */
+export const signInWithGoogle = async () => {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+};
+
+/**
+ * Signs the user out.
+ * @returns A Promise that resolves when the user is signed out.
+ */
+export const signOut = async () => {
+  return auth.signOut();
+};
+
+/**
+ * A helper function that listens for auth state changes and triggers a callback when it changes.
+ * @param callback The callback function to call when the auth state changes.
+ */
+export const onAuthStateChangedHelper = (callback: (user: User | null) => void) => {
+  return onAuthStateChanged(auth, callback);
+};
