bed2a1bc039e8ea0dc67f78bb54968da9656b562
Hubert Madej <hubertmadej02@gmail.com>
2024-04-30 20:58:51 +0200

    Added function for generating signed url for file uploads.

--- api-service/functions/src/index.ts
+++ api-service/functions/src/index.ts
@@ -2,9 +2,14 @@ import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 const fireStore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "vidspark-raw-videos";
 
 /**
  * Function to create a new user in firestore.
@@ -20,4 +25,26 @@ export const createUser = functions.auth.user().onCreate((user) => {
   fireStore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User created: ${JSON.stringify(userInfo)}`);
   return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if user is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "Unauthenticated access to the function."
+    );
+  }
+  const {auth, data} = request;
+  const bucket = storage.bucket(rawVideoBucketName);
+  // Generate a unique file name.
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
 });
